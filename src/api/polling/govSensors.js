12532c8a5db7401fcf1e63457ad02c4374a1192a
Rosie Watson <rosie.watson@holidayextras.com>
2018-11-28 20:47:31 +0000

    WIP gov sensor api poll

--- src/api/polling/govSensors.js
+++ src/api/polling/govSensors.js
@@ -1,6 +1,7 @@
 const govFloods = module.exports = {}
 const request = require('request')
 const util = require('util')
+const db = ('../../lib/database.js')
 
 govFloods.config = {
   pollingDelay: 15 * 1000
@@ -9,12 +10,29 @@ govFloods.config = {
 govFloods.fetchAndStore = async () => {
   let res
   try {
-    res = await util.promisify(request.get)('https://environment.data.gov.uk/flood-monitoring/data/readings?_view=full&latest&parameter=level')
+    // res = await util.promisify(request.get)('https://environment.data.gov.uk/flood-monitoring/data/readings?_view=full&latest&parameter=level')
   } catch (e) {
     console.log(e)
   }
-  const json = JSON.parse(res.body)
-  console.log(json.meta)
+  const json = require('./fakereq')
+
+  json.items.forEach(item => {
+    let row = []
+
+    row.push(item['@id'].split('/').reverse()[1])
+    row.push(item.dateTime)
+    row.push(item.measure.parameter)
+    row.push(item.measure.qualifier)
+    row.push(item.measure.station['@id'].split('/').reverse()[0])
+    row.push(item.measure.station.label)
+    row.push(item.value)
+    row.push(item.measure.unitName)
+    row.push(item.measure.valueType)
+
+    await db.query()
+
+    console.log('row', row)
+  })
 }
 
 ;(() => {
