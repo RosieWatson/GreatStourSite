cb9f252e3b076527d9048fd4cff0134a2bd40b1f
YcleptJohn <john.taylor@holidayextras.com>
2018-11-28 19:43:39 +0000

    Actually pulling the gov flood warnings and structure for govSensors

---
+++ src/lib/polling/govSensors.js
@@ -0,0 +1,11 @@
+const govFloods = module.exports = {}
+const request = require('request')
+
+
+govFloods.config = {
+  pollingDelay: 15 * 1000;
+}
+
+govFloods.fetchAndStore = () => {
+  console.log('govSensors:fetchAndStore unimplemented')
+}
