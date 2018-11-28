1da30126739b3a3e549e4327e512bec2859f080d
YcleptJohn <john.taylor@holidayextras.com>
2018-11-28 19:43:39 +0000

    Actually pulling the gov flood warnings and structure for govSensors

---
+++ src/lib/polling/govFloods.js
@@ -0,0 +1,23 @@
+const govFloods = module.exports = {}
+const request = require('request')
+const util = require('util')
+
+
+govFloods.config = {
+  pollingDelay: 15 * 1000
+}
+
+govFloods.fetchAndStore = async () => {
+  let res
+  try {
+    res = await util.promisify(request.get)('https://environment.data.gov.uk/flood-monitoring/id/floods')
+  } catch (e) {
+    console.log(e)
+  }
+  const json = JSON.parse(res.body)
+  console.log(json.meta)
+}
+
+;(() => {
+  govFloods.fetchAndStore()
+})()
