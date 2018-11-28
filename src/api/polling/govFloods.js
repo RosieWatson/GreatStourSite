8398951acdf79fb19f969713db1a19b4655e5544
YcleptJohn <john.taylor@holidayextras.com>
2018-11-28 20:49:26 +0000

    Add polling and pushing to the database - WIP

--- src/api/polling/govFloods.js
+++ src/api/polling/govFloods.js
@@ -1,6 +1,7 @@
 const govFloods = module.exports = {}
 const request = require('request')
 const util = require('util')
+const db = require('../../lib/database.js')
 
 
 govFloods.config = {
@@ -13,9 +14,26 @@ govFloods.fetchAndStore = async () => {
     res = await util.promisify(request.get)('https://environment.data.gov.uk/flood-monitoring/id/floods')
   } catch (e) {
     console.log(e)
+    return
   }
   const json = JSON.parse(res.body)
-  console.log(json.meta)
+  for (i of json.items) {
+    let row = []
+    row.push(i['@id'].split('/').reverse()[0])
+    row.push(Date.now())
+    row.push(i.floodArea.riverOrSea)
+    row.push(i.eaAreaName)
+    row.push(i.eaRegionName)
+    row.push(JSON.stringify(i.floodArea.county.split('').join('')))
+    row.push(i.description)
+
+    await db.query(`
+      INSERT into govFloods (id, timestamp, waterbody, eaAreaName, eaRegionName, counties, description)
+      VALUES (?, ?, ?, ?, ?, ?, ?)
+    `, row)
+    console.log('didit')
+    process.exit(0)
+  }
 }
 
 ;(() => {
