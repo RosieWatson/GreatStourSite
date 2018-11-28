2d2e6c2816cf431afffc531d79086fdd469f823e
Rosie Watson <rosie.watson@holidayextras.com>
2018-11-28 18:00:01 +0000

    Unused mqtt for now

--- src/lib/mqtt.js
+++ src/lib/mqtt.js
@@ -1,14 +1,15 @@
-const mqtt = require('mqtt')
-const client  = mqtt.connect('mqtt://eu.thethings.network', {username: 'kentwatersensors', password: 'ttn-account-v2.7j6Z9OduNwFW7il2Sd28YYF4Q-8l9rDDPaNRFw06-GM'})
-const subTopic = 'kentwatersensors/devices/+/up'
+// const mqtt = require('mqtt')
+// const subTopic = 'kentwatersensors/devices/+/up'
 
-client.on('connect', () => {
-  console.log('conected')
-  client.subscribe(subTopic)
-})
+// client.on('connect', () => {
+//   console.log('conected')
+//   client.subscribe(subTopic)
+// })
 
-client.on('message', (topic, message) => {
-  console.log('Message:')
-  console.log(topic.toString())
-  console.log(message.toString())
-})
+// client.on('message', (topic, message) => {
+//   console.log('Message:')
+//   console.log(topic.toString())
+//   console.log(message.toString())
+
+//   // message.payload_raw (base64 -> hex -> decimal)
+// })
