c970994a9baf3bc6f85f0bec4d5c12dfb3fda745
Rosie Watson <rosie.watson@holidayextras.com>
2018-11-29 11:31:27 +0000

    Polling gov for sensor data then storing in db

--- src/api/polling/govSensors.js
+++ src/api/polling/govSensors.js
@@ -1,7 +1,7 @@
 const govFloods = module.exports = {}
 const request = require('request')
 const util = require('util')
-const db = ('../../lib/database.js')
+const db = require('../../lib/database.js')
 
 govFloods.config = {
   pollingDelay: 15 * 1000
@@ -10,31 +10,38 @@ govFloods.config = {
 govFloods.fetchAndStore = async () => {
   let res
   try {
-    // res = await util.promisify(request.get)('https://environment.data.gov.uk/flood-monitoring/data/readings?_view=full&latest&parameter=level')
+    res = await util.promisify(request.get)('https://environment.data.gov.uk/flood-monitoring/data/readings?_view=full&latest&parameter=level')
   } catch (e) {
     console.log(e)
   }
-  const json = require('./fakereq')
+  const json = JSON.parse(res.body)
 
-  json.items.forEach(item => {
+  for (item of json.items) {
     let row = []
 
     row.push(item['@id'].split('/').reverse()[1])
-    row.push(item.dateTime)
+    row.push((item.dateTime).slice(0, -1))
     row.push(item.measure.parameter)
     row.push(item.measure.qualifier)
     row.push(item.measure.station['@id'].split('/').reverse()[0])
-    row.push(item.measure.station.label)
+    let label = item.measure.station.label
+    if (Array.isArray(label)) {
+      row.push(label.join(', '))
+    } else {
+      row.push(label)
+    }
     row.push(item.value)
     row.push(item.measure.unitName)
     row.push(item.measure.valueType)
 
-    await db.query()
-
-    console.log('row', row)
-  })
+    await db.query(`
+      INSERT into govSensors (id, timestamp, parameter, qualifier, stationId, stationLabel, value, unitName, valueType)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `, row)
+  }
+  process.exit(0)
 }
 
-;(() => {
+(() => {
   govFloods.fetchAndStore()
 })()
