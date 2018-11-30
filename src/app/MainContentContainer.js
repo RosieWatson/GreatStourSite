6e2e9f22455a2a5b5ac9548dd1d689bf1909945b
Jean-Luc Sorak <jlsorak@icloud.com>
2018-11-30 23:40:37 +0000

    React Maps

--- src/app/MainContentContainer.js
+++ src/app/MainContentContainer.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import { Layout } from 'antd';
 
+import Map from './components/Map'
+
 class MainContentContainer extends React.Component {
   render () {
     return (
       <Layout.Content>
-        <div id="content-container">
-          <div id="map-container">
-            <div id="map-overlay">
-              
-            </div>
-            
-            <div id="map">
-              
-            </div>
-          </div>
-          
-        </div>
+        <Map 
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBetLRAD6Cy4rbI9fDtzwIXdWB-C3FB6XA&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          />
       </Layout.Content>
     )
   }
