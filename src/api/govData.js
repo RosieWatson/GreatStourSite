1e1078a463f35bac0042d21a03ef601f8b1d9dc6
YcleptJohn <john.taylor@holidayextras.com>
2018-12-03 17:15:44 +0000

    Add skeleton of the govData api

---
+++ src/api/govData.js
@@ -0,0 +1,5 @@
+const app = require('../server.js')
+const database = require('../lib/database.js')
+
+app.get('/api/govdata/fetch/floods', async (req, res) => {})
+app.get('/api/govdata/fetch/sensors', async (req, res) => {})
