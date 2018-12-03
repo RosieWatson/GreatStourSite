1d9cdf86b21d974b4fcc3928d3394c9aa303a48a
YcleptJohn <john.taylor@holidayextras.com>
2018-12-03 17:15:25 +0000

    Add express app support for our API, including token checks

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
-const app = express()
+const app = module.exports = express() // Export the express app for use by the API sub-files
 // const mqtt = require('./lib/mqtt')
 const pollExecutor = require('./api/pollExecutor.js')
 
 pollExecutor.startAll() // WILL YOU START THE POLLS PLEASEEEE
 app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.json())
 
 app.get('/ping', function (req, res) {
  return res.send('pong')
@@ -15,5 +16,17 @@ app.get('/ping', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
+
+app.route('/api/*') // All API routes must provide the API token
+  .get((req, res, next) => {
+    if (req && req.query && req.query.token !== process.env.API_TOKEN) return res.sendStatus(403)
+    next()
+  })
+  .post((req, res, next) => {
+    if (req && req.body && req.body.token !== process.env.API_TOKEN) return res.sendStatus(403)
+    next()
+  })
+
+require('./api/govData.js')
 
 app.listen(process.env.PORT || 3000)
