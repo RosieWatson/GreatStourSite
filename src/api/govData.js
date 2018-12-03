48ad2f7c39ca022a38754d604b69818ba73c5535
YcleptJohn <john.taylor@holidayextras.com>
2018-12-03 17:50:06 +0000

    Add basic fetchable endpoints for the govdata (sensor+flood)

--- src/api/govData.js
+++ src/api/govData.js
@@ -1,12 +1,28 @@
 const app = require('../server.js')
-const database = require('../lib/database.js')
+const db = require('../lib/database.js')
 
 app.get('/api/govdata/fetch/floods', async (req, res) => {
-  // SELECT * FROM govFloods WHERE waterbody = 'Great Stour';
-})
+  let result
+  try {
+    result = await db.query(`SELECT * FROM govFloods;`, [])
+  } catch (e) {
+    console.log('Error during FLOODS FETCH')
+    console.log(e)
+  }
+  console.log('Result')
+  console.log(result)
+  return res.send(result)
+ })
 
 app.get('/api/govdata/fetch/sensors', async (req, res) => {
-  // SELECT * FROM govSensors gSens
-  // JOIN govStations gStat
-  // ON gSens.id = gStat.id;
+  let result
+  try {
+    result = await db.query(`SELECT * FROM govSensors gSens JOIN govStations gStat ON gSens.id = gStat.id`)
+  } catch (e) {
+    console.log('ERROR DURING SENSORS FETCH')
+    console.log(e)
+  }
+  console.log('Result')
+  console.log(result)
+  return res.send(result)
 })
