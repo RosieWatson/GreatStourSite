7d8075c6b9dd8803e29c88459b156f7565c725c9
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-03 17:36:52 +0000

    SQL queries to get govData

--- src/api/govData.js
+++ src/api/govData.js
@@ -1,5 +1,12 @@
 const app = require('../server.js')
 const database = require('../lib/database.js')
 
-app.get('/api/govdata/fetch/floods', async (req, res) => {})
-app.get('/api/govdata/fetch/sensors', async (req, res) => {})
+app.get('/api/govdata/fetch/floods', async (req, res) => {
+  // SELECT * FROM govFloods WHERE waterbody = 'Great Stour';
+})
+
+app.get('/api/govdata/fetch/sensors', async (req, res) => {
+  // SELECT * FROM govSensors gSens
+  // JOIN govStations gStat
+  // ON gSens.id = gStat.id;
+})
