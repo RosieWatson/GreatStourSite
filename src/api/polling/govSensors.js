d134eacf94bb5ff341d7de1b172f86f230f389ca
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-03 15:04:04 +0000

    Tidying up console logs

--- src/api/polling/govSensors.js
+++ src/api/polling/govSensors.js
@@ -2,7 +2,6 @@ const govFloods = module.exports = {}
 const request = require('request')
 const util = require('util')
 const db = require('../../lib/database.js')
-// const stationIds = ['E3826', 'E3951', 'E3966']
 const stationIds = ['E3826-level-stage-i-15_min-mAOD', 'E3951-level-stage-i-15_min-mASD', 'E3966-level-stage-i-15_min-mASD']
 
 govFloods.config = {
@@ -14,14 +13,12 @@ govFloods.fetchAndStore = async () => {
     let res
 
     try {
-      // res = await util.promisify(request.get)(`https://environment.data.gov.uk/flood-monitoring/id/stations/${station}/readings?latest`)
       res = await util.promisify(request.get)(`https://environment.data.gov.uk/flood-monitoring/id/measures/${station}`)
     } catch (e) {
       console.log(e) // need to do some handling to report api down or something
     }
 
     const json = JSON.parse(res.body)
-    console.log('My JSON: ', json)
     const item = json.items
     let row = []
 
@@ -39,7 +36,6 @@ govFloods.fetchAndStore = async () => {
     row.push(item.latestReading.value)
     row.push(item.unitName)
     row.push(item.valueType)
-    console.log('My row: ', row)
     await db.query(`
       INSERT into govSensors (id, timestamp, parameter, qualifier, stationId, stationLabel, value, unitName, valueType)
       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
