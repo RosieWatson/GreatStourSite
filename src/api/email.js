488b083d5e29b04a6b2bdfd599f95c2300bac60c
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-04 12:03:21 +0000

    Adding in API endpoint for welcome email

---
+++ src/api/email.js
@@ -0,0 +1,16 @@
+const app = require('../server.js')
+const emailGen = require('../lib/email/generator.js')
+const emailTrans = require('../lib/email/transporter.js')
+
+app.post('/api/email/welcome', async (req, res) => {
+  try {
+    console.log('the req', req.body)
+    let emailTemp = await emailGen.createWelcomeEmail(req.body.name)
+    await emailTrans.sendEmail(req.body.email, 'Welcome to Great Stour Flood Alerts', emailTemp)
+
+    return res.status(200).send('OK')
+  } catch (e) {
+    console.log(e)
+    return res.status(500).send('Error sending welcome email')
+  }
+})
--- src/server.js
+++ src/server.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
-const app = express()
+const app = module.exports = express() // Export the express app for use by the API sub-files
 // const mqtt = require('./lib/mqtt')
 const pollExecutor = require('./api/pollExecutor.js')
 
 pollExecutor.startAll() // WILL YOU START THE POLLS PLEASEEEE
 app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('/ping', function (req, res) {
- return res.send('pong')
-})
+app.use(express.json())
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
+
+require('./api/email.js')
 
 app.listen(process.env.PORT || 3000)
