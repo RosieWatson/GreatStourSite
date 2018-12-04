51ead882c46385f8afc3cdeb13b0c006719d77dd
Christian Dalby <itechix@gmail.com>
2018-12-04 21:40:02 +0000

    Restructured sider for mobile view collapse

--- src/app/SidebarContainer.js
+++ src/app/SidebarContainer.js
@@ -1,16 +1,68 @@
 import React from 'react'
 
-import { Layout, Button, Input, Row, Col, DatePicker } from 'antd'
+import { Layout, Button, Input, Row, Col, DatePicker, Collapse } from 'antd'
 
 class SidebarContainer extends React.Component {
     render () {
-    
-        return (
-            <Layout.Sider id="sider-root"
-                collapsible
-                reverseArrow
-                width="30%">
+        const Panel = Collapse.Panel;
 
+        return (
+            <Layout.Sider collapsible collapsedWidth={48}
+                    reverseArrow>
+
+                <div id="details-header">
+                    <h1>Details</h1>
+                </div>
+
+                <Collapse accordion>
+                    <Panel header="This is panel header 1" key="1">
+                        <p>ddfd</p>
+                    </Panel>
+                    <Panel header="This is panel header 2" key="2">
+                        <p>fdfd</p>
+                    </Panel>
+                    <Panel header="This is panel header 3" key="3">
+                        <p>tex</p>
+                    </Panel>
+                    <Panel header="This is panel header 3" key="3">
+                        <p>tex</p>
+                    </Panel>
+                    <Panel header="This is panel header 3" key="3">
+                        <p>tex</p>
+                    </Panel>
+                    <Panel header="This is panel header 3" key="3">
+                        <p>tex</p>
+                    </Panel>
+                    <Panel header="This is panel header 3" key="3">
+                        <p>tex</p>
+                    </Panel>
+                    <Panel header="This is panel header 3" key="3">
+                        <p>tex</p>
+                    </Panel><Panel header="This is panel header 3" key="3">
+                    <p>tex</p>
+                </Panel><Panel header="This is panel header 3" key="3">
+                    <p>tex</p>
+                </Panel><Panel header="This is panel header 3" key="3">
+                    <p>tex</p>
+                </Panel>
+                    <Panel header="This is panel header 3" key="3">
+                        <p>tex</p>
+                    </Panel>
+                    <Panel header="This is panel header 3" key="3">
+                        <p>tex</p>
+                    </Panel>
+                    <Panel header="This is panel header 3" key="3">
+                        <p>tex</p>
+                    </Panel><Panel header="This is panel header 3" key="3">
+                    <p>tex</p>
+                </Panel><Panel header="This is panel header 3" key="3">
+                    <p>tex</p>
+                </Panel><Panel header="This is panel header 3" key="3">
+                    <p>tex</p>
+                </Panel>
+
+
+                </Collapse>
             </Layout.Sider>
         )
     }
