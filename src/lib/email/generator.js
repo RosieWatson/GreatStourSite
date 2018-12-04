0b618065469127aaf9fc5cb079d5757d45988c30
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-04 11:17:40 +0000

    Refactoring email generator into welcome function

--- src/lib/email/generator.js
+++ src/lib/email/generator.js
@@ -1,16 +1,17 @@
 const Mailgen = require('mailgen')
- 
+
 const mailGenerator = new Mailgen({
-    theme: 'default',
-    product: {
-        name: 'Great Stour Site',
-        link: 'localhost:8080'
-    }
+  theme: 'default',
+  product: {
+      name: 'Great Stour Site',
+      link: 'localhost:8080'
+  }
 })
 
-const welcomeEmail = {
-  body: {
-      name: 'Test Name',
+generator.createWelcomeEmail = name => {
+  let email = {
+    body: {
+      name: name,
       intro: 'Thank you for signing up to Great Stour flood alerts.',
       action: {
         instructions: 'In the mean time, if you would like to see any information on current (or historic water level), please visit:',
@@ -20,10 +21,11 @@ const welcomeEmail = {
             link: 'localhost:8080'
         }
       },
+    }
   }
+
+  let HTMLEmail = mailGenerator.generate(email.createWelcomeEmail())
+  let textEmail = mailGenerator.generatePlaintext(email.createWelcomeEmail()) // To support people who don't allow HTML
+
+  return { HTMLEmail, textEmail }
 }
-
-const HTMLEmail = mailGenerator.generate(welcomeEmail)
-const textEmail = mailGenerator.generatePlaintext(welcomeEmail) // To support people who don't allow HTML
-
-require('fs').writeFileSync('preview.html', HTMLEmail, 'utf8') // Temporary to view the email
