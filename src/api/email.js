2f9de0a70d1e981a92627fb1334217cbd17a2775
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-05 15:38:36 +0000

    Adding API endpoints from adding and removing users

--- src/api/email.js
+++ src/api/email.js
@@ -1,8 +1,16 @@
 const app = require('../server.js')
 const emailGen = require('../lib/email/generator.js')
 const emailTrans = require('../lib/email/transporter.js')
+const subscribers = require('../lib/email/subscribers.js')
 
-app.post('/api/email/welcome', async (req, res) => {
+app.post('/api/email/user/subscribe', async (req, res) => {
+  try {
+    await subscribers.addUser(res.body)
+  } catch (e) {
+    console.log(e)
+    return res.status(500).send('Error: failed to add user to database')
+  }
+
   try {
     let emailTemp = await emailGen.createWelcomeEmail(req.body.name)
     await emailTrans.sendEmail(req.body.email, 'Welcome to Great Stour Flood Alerts', emailTemp)
@@ -10,6 +18,16 @@ app.post('/api/email/welcome', async (req, res) => {
     return res.status(200).send('OK')
   } catch (e) {
     console.log(e)
-    return res.status(500).send('Error sending welcome email')
+    return res.status(500).send('Error: failed to send welcome email')
+  }
+})
+
+app.post('/api/email/user/unsubscribe', async (req, res) => {
+  try {
+    await subscribers.removeUser(res.body.email)
+    return res.status(200).send('OK')
+  } catch (e) {
+    console.log(e)
+    return res.status(500).send('Error: failed to remove user from database')
   }
 })
