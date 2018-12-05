73eee40f29c9459b0993a1b033bc89c884432e2e
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-05 16:36:34 +0000

    Remove unused server ping function

--- src/server.js
+++ src/server.js
@@ -9,10 +9,6 @@ pollExecutor.startAll()
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.json())
 
-app.get('/ping', function (req, res) {
- return res.send('pong')
-})
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
