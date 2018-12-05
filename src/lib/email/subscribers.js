09416b4195b545bf5cd629f9b950d7442eac91ed
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-05 15:12:00 +0000

    Adding function to remove users from the DB

--- src/lib/email/subscribers.js
+++ src/lib/email/subscribers.js
@@ -2,26 +2,37 @@ const subscribers = module.exports = {}
 const db = require('../database.js')
 
 subscribers.addUser = async data => {
-  let name = data.name
-  let email = data.email
-  let postcode = data.postcode
-  let sensor = data.sensor
+  try {
+    let row = []
 
+    row.push(parseInt((Date.now() + '').slice(0,-3)))
+    row.push(data.name)
+    row.push(data.email)
+    row.push(data.postcode)
+    row.push(data.sensor)
 
-  let row = []
-
-  row.push(parseInt((Date.now() + '').slice(0,-3)))
-  row.push(name)
-  row.push(email)
-  row.push(postcode)
-  row.push(sensor)
-
-  await db.query(`
+    await db.query(`
       INSERT IGNORE into subscribers (timestamp, name, email, postcode, sensor)
       VALUES (?, ?, ?, ?, ?)
     `, row)
+  } catch (e) {
+    console.log('Couldn\'t insert user into database')
+    console.log(e)
+  }
+}
+
+subscribers.removeUser = async email => {
+  try {
+    await db.query(`
+      DELETE FROM subscribers WHERE email = ?
+    `, email)
+  } catch (e) {
+    console.log('Couldn\'t remove user from database')
+    console.log(e)
+  }
 }
 
 ;(() => {
-  subscribers.addUser({name: 'mr test test', email: 'test@test.com', postcode: 'testcode', sensor: 'testsensor'})
+  // subscribers.addUser({name: 'mr test test', email: 'test@test.com', postcode: 'testcode', sensor: 'testsensor'})
+  subscribers.removeUser('test@test.com')
 })()
