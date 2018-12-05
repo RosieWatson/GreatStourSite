68011ab2a85ae105f580bc3a21aaf81bfb24c9b1
YcleptJohn <john.taylor@holidayextras.com>
2018-12-05 11:56:02 +0000

    Centralised error translations for ease of user clarity

---
+++ src/lib/errors.js
@@ -0,0 +1,24 @@
+const errors = module.export = {}
+
+const definedErrors = [
+  {
+    short: 'FAILED_GOVFLOODS_LOOKUP',
+    long: 'Something went wrong trying to lookup flood data from our database.'
+  },
+  {
+    short: 'FAILED_GOVSENSORS_LOOKUP',
+    long: 'Something went wrong trying to lookup river sensor data from our database.'
+  }
+]
+
+// Return the pretty format version of a defined error (lookup via shortError message)
+// Allows the API to return short errors which can be translated to long errors for users
+errors.pretty = (shortError) => {
+  let r = null
+  r = definedErrors.find(e => e.short === shortError)
+  return (r && r.long) || 'Something went wrong...'
+}
+
+;(() => {
+  console.log(errors.pretty('FAILED_GOVFLOODS_LOOKUP'))
+})()
