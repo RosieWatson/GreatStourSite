e64c3cf2f31e9cfc265cb349847fefbb287643fa
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-05 14:50:50 +0000

    Adding function to insert users into the DB

--- src/lib/email/subscribers.js
+++ src/lib/email/subscribers.js
@@ -1,5 +1,27 @@
 const subscribers = module.exports = {}
+const db = require('../database.js')
 
-subscribers.addUser = data => {
+subscribers.addUser = async data => {
+  let name = data.name
+  let email = data.email
+  let postcode = data.postcode
+  let sensor = data.sensor
 
+
+  let row = []
+
+  row.push(parseInt((Date.now() + '').slice(0,-3)))
+  row.push(name)
+  row.push(email)
+  row.push(postcode)
+  row.push(sensor)
+
+  await db.query(`
+      INSERT IGNORE into subscribers (timestamp, name, email, postcode, sensor)
+      VALUES (?, ?, ?, ?, ?)
+    `, row)
 }
+
+;(() => {
+  subscribers.addUser({name: 'mr test test', email: 'test@test.com', postcode: 'testcode', sensor: 'testsensor'})
+})()
