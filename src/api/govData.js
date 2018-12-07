ee7c93aca3578c0fc088ec1b4134b34f452da6d7
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-07 15:19:00 +0000

    Beautifying the file to fix layout

--- src/api/govData.js
+++ src/api/govData.js
@@ -7,12 +7,18 @@ withinRefreshCheck = async table => {
 
   try {
     result = await db.query(`SELECT max(timestamp) AS oldestResult FROM ${table};`)
-    if(new Date(result[0].oldestResult) > new Date(Date.now() - (60 * 15 * 1000))) return { errors, data: true }
+    if (new Date(result[0].oldestResult) > new Date(Date.now() - (60 * 15 * 1000))) return {
+      errors,
+      data: true
+    }
   } catch (e) {
     console.log('Error occurred during refresh quota timecheck')
     errors.push('FAILED_REFRESH_QUOTA_CHECK')
   } finally {
-    return { errors, data: false }
+    return {
+      errors,
+      data: false
+    }
   }
 }
 
@@ -38,15 +44,11 @@ app.get('/api/govdata/fetch/sensors', async (req, res) => {
   let errors = []
   let result = null
   try {
-    result = await db.query(`
-                              SELECT gSens.*, gStat.latitude, gStat.longitude, gStat.description
-                              FROM govSensors gSens
-                              JOIN govStations gStat
-                              ON gSens.id = gStat.id
-                              WHERE gSens.timestamp = (SELECT MAX(gSens2.timestamp)
-                                                          FROM govSensors gSens2
-                                                          WHERE gSens2.id = gSens.id)
-                            `)
+    result = await db.query(
+      `SELECT gSens.*, gStat.latitude, gStat.longitude, gStat.description FROM govSensors gSens
+        JOIN govStations gStat ON gSens.id = gStat.id
+        WHERE gSens.timestamp = (SELECT MAX(gSens2.timestamp) FROM govSensors gSens2 WHERE gSens2.id = gSens.id)
+       `)
   } catch (e) {
     console.log('Failed to fetch data from govFloods table', e)
     errors.push('FAILED_GOVSENSORS_LOOKUP')
@@ -66,7 +68,10 @@ app.get('/api/govdata/fetch/last30days', async (req, res) => {
   let currentDate = req.body.date + ' 23:59:59'
 
   try {
-    result = await db.query(`SELECT * FROM riverData.govSensors WHERE id = ? AND latestReading BETWEEN ? - INTERVAL 30 DAY AND ?`, [req.body.stationID, currentDate, currentDate])
+    result = await db.query(
+      `SELECT * FROM riverData.govSensors WHERE id = ? AND latestReading BETWEEN ? - INTERVAL 30 DAY AND ?`,
+      [req.body.stationID, currentDate, currentDate]
+    )
   } catch (e) {
     console.log('Failed to fetch data from govSensors table', e)
     errors.push('FAILED_GOVSENSORS_LOOKUP')
