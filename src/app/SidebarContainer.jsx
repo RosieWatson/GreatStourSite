537bde1881f9cf27e85fbd3773e0fd8a52c1af5f
Jean-Luc Sorak <jlsorak@icloud.com>
2018-12-08 02:32:32 +0000

    Make map interactive

--- src/app/SidebarContainer.jsx
+++ src/app/SidebarContainer.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { Alert, Layout, Button, Input, Row, Col, DatePicker, Collapse } from 'antd'
+import ReactDOM from 'react-dom'
 
 import SystemAvailability from './components/SystemAvailability'
 
 class SidebarContainer extends React.Component {
-  render () {
+  constructor(props) {
+    super(props)
+      
+    // Needed to make the antd 'collapse' component fully keyboard accessible
+    // We remove this element from the tab-index and then add a tab-index to the element 
+    // we do want to have keyboard focus further below
+    this.removeFromTabIndex = element => {
+      ReactDOM.findDOMNode(element).firstChild.tabIndex = '-1'
+    };
+  } 
+  
+  render() {
     const Panel = Collapse.Panel
-    const { sensorData, systemAvailability } = this.props
+    const { selectSensor, sensorData, systemAvailability } = this.props
+
     return (
       <Layout.Sider 
         collapsible 
@@ -17,18 +30,38 @@ class SidebarContainer extends React.Component {
           <SystemAvailability systemAvailability={systemAvailability} />
         </div>
         
-        <Collapse accordion>
-          {sensorData.length && sensorData.map((sensor, index) => {
-            return (
-              <Panel header={`${sensor.description} - ${sensor.deviceID || sensor.id}`} key={`sensor-list-item-${index}`}>
-                <p>{sensor.stationId}</p>
-              </Panel>
-            )
-          })}
-        </Collapse>
+        <div id='sensor-sidebar'>
+          <Collapse accordion activeKey={`sensor-list-item-${this.props.selectedSensor}`}>
+            {sensorData.length && sensorData.map((sensor) => {
+              return (
+                <Panel 
+                  header={panelHeader(sensor, selectSensor)}
+                  key={`sensor-list-item-${sensor.id}`}
+                  ref={this.removeFromTabIndex}
+                  showArrow={false}
+                  >
+                  <p>{sensor.stationId}</p>
+                </Panel>
+              )
+            })}
+          </Collapse>
+        </div>
       </Layout.Sider>
     )
   }
+}
+
+const panelHeader = (sensor, selectSensor) => {
+  return (
+    <div 
+      tabIndex={0} 
+      onClick={() => selectSensor(sensor.id)}
+      onKeyDown={(e) => e.key === 'Enter' && selectSensor(sensor.id)}
+      role='button'
+      >
+      {`${sensor.description} - ${sensor.deviceID || sensor.id}`}
+    </div>
+  )
 }
 
 export default SidebarContainer
