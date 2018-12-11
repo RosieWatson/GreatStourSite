efcd6032670306f8987796693452ce121c5f5fb2
Christian Dalby <itechix@gmail.com>
2018-12-11 20:20:32 +0000

    Added postcode validation / regex

--- src/app/components/SubscribeModal.jsx
+++ src/app/components/SubscribeModal.jsx
@@ -127,6 +127,9 @@ const SubscribeCreateForm = Form.create()(
                 }, {
                   required: true,
                   message: 'Please input your postcode!',
+                }, {
+                  pattern: '([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})',
+                  message: 'The postcode does not match the expected format!'
                 }],
               })(
                 <Input/>
