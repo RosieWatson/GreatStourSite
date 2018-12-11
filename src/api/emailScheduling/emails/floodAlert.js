fe53ffa45e6f3cfa978e6f49d39b49a7fde205e8
YcleptJohn <john.taylor@holidayextras.com>
2018-12-11 15:21:33 +0000

    Start calculating and storing alert states per sub

--- src/api/emailScheduling/emails/floodAlert.js
+++ src/api/emailScheduling/emails/floodAlert.js
@@ -24,6 +24,36 @@ floodAlert.getCountyFromPostcode = async (postcode) => {
   return res.result.admin_county
 }
 
+floodAlert.floodStatesToJson = (floods) => {
+  if (!floods) return {}
+  let json = {}
+  floods.forEach(f => {
+    json[f.id] = {
+      severityLevel: f.severityLevel
+    }
+  })
+  return json
+}
+
+// Calculates which chnges have happened to flood severity between two floodState objects
+// Used to only email when a state has changed according to the last state we sent them
+floodAlert.deduceNew = (oldSeverities, currSeverities) => {
+  let updated = {}
+  const allKeys = Array.from(new Set([].concat(Object.keys(oldSeverities), Object.keys(currSeverities))))
+
+  allKeys.forEach(k => {
+    // Discard the data if the newest flood warning doesn't overlap with the old one
+    if ((oldSeverities[k] && oldSeverities[k].severityLevel) && !currSeverities[k]) return
+
+    // If a flood warning exists in the current but not the old model we want to keep it
+    if ((currSeverities[k] && currSeverities[k].severityLevel) && !oldSeverities[k]) return updated[k] = currSeverities[k]
+
+    // If a flood exists in both old+new we only want to store it if the level has changed in the new state
+    if (oldSeverities[k].severityLevel !== currSeverities[k].severityLevel) return updated[k] = currSeverities[k]
+  })
+  return updated
+}
+
 floodAlert.checkAndDispatch = async () => {
   let subscribed
   try {
@@ -44,6 +74,15 @@ floodAlert.checkAndDispatch = async () => {
       return
     }
     if (localFloods.length < 1) return // No floods near this subscriber
+    // Turn to JSON ✅
+    // Loop keys ✅
+    // find difference between old status and current (json diffing) (reduce list into new changes) ✅
+    // If there is no difference, stop ✅
+    // email resulting difference as a single email
+    // Update the subscriber's lastAlerted and LastStates in db
+
+    const changed = floodAlert.deduceNew(JSON.parse(s.lastAlertStates), floodAlert.floodStatesToJson(localFloods))
+
     const emailContent = emailGenerator.createFloodAlertEmail(s, localFloods)
     const targetEmail = subscribers.decryptEmail(s.email)
     emailTransport.sendEmail(targetEmail, 'Flood Alert for your area!', emailContent)
