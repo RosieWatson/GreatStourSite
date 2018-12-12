f98de12ab781d4f2e06461301682fef39a14c6e3
Christian Dalby <itechix@gmail.com>
2018-12-12 11:10:42 +0000

    Add new markers to map - currently refreshes on click :/

--- src/app/components/Map.jsx
+++ src/app/components/Map.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import GoogleMapReact from 'google-map-react'
 import { DatePicker } from 'antd'
+import moment from 'moment'
+import axios from 'axios'
 
 import Search from './Search'
 import Marker from './Marker'
@@ -8,6 +10,7 @@ import SubscribeModal from './SubscribeModal'
 import UnsubscribeModal from "./UnsubscribeModal";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
+let geocoder
 
 class Map extends Component {
   constructor(props) {
@@ -16,7 +19,13 @@ class Map extends Component {
     this.state = {
       mapInstance: null,
       mapApi: null,
+      sensorData: props.sensorData
     };
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({sensorData: props.sensorData});
+    console.log("data:", this.state.sensorData)
   }
   
   static defaultProps = {
@@ -30,16 +39,70 @@ class Map extends Component {
       mapApi: maps,
     }, this.props.setMapApiLoaded);
   };
-  
+
+  // Get all sensor data for a specific day
+  getSensorData(date) {
+    Promise.all([
+      axios.post('api/govdata/fetch/specificDate', {
+        date: date
+      }),
+      axios.post('api/mqttdata/fetch/specificDate', {
+        date: date
+      })
+    ]).then(([govData, mqttData]) => {
+      // Reverse Geocode the address for the MQTT sensors
+      geocoder = !geocoder ? new google.maps.Geocoder : geocoder;
+      const mqttSensorData = mqttData.data.data;
+      Promise.all(mqttSensorData.map(async (sensor) => {
+        const address = await this.reverseGeocode(geocoder, sensor.latitude, sensor.longitude)
+        return Object.assign({description: address}, sensor)
+      }))
+        .then((mqttSensorDataWithAddress) => {
+          // Merge the MQTT sensor data with the Gov sensor data
+          const sensorData = govData.data.data.concat(mqttSensorDataWithAddress)
+          this.setState({
+            sensorData: sensorData
+          })
+        })
+    })
+  }
+
+  // Gets the address according to a set of coordinates
+  reverseGeocode(geocoder, latitude, longitude) {
+    return new Promise((resolve, reject) => {
+      geocoder.geocode({'location': {lat: latitude, lng: longitude}}, (results, status) => {
+        if(status === 'OK') {
+          const address = results.find((result) => {
+            return result.types.includes('route')
+          })
+          resolve(address ? address.address_components[0].long_name : '')
+        } else {
+          reject()
+        }
+      })
+    })
+  }
+
+  dateChange = (d, dString) => {
+    // replace sensorData
+    console.log(dString)
+    this.getSensorData(dString)
+    this.setState(this.state)
+    console.log(this.state.sensorData)
+
+    console.log("map updated")
+
+  }
+
   render() {
-    const { places, mapInstance, mapApi } = this.state
+    const { places, mapInstance, mapApi, sensorData } = this.state
     const { mapApiLoaded } = this.props
     
     return (
       <div id='map-container'>
         <div id='map-utility'>
           {mapApiLoaded && <Search map={mapInstance} mapApi={mapApi} addplace={this.addPlace} />}
-          <DatePicker />
+          <DatePicker value={moment()} onChange={this.dateChange}/>
           <a className='skip-link' href='#sidebar'>Skip past map</a>
           <div id='button-utility'>
             <SubscribeModal/>
@@ -56,7 +119,7 @@ class Map extends Component {
           onGoogleApiLoaded={({ map, maps }) => this.apiHasLoaded(map, maps)}
           yesIWantToUseGoogleMapApiInternals={true} 
           >
-          {this.props.sensorData.length && this.props.sensorData.map((sensor, id) => (
+          {sensorData.length && sensorData.map((sensor, id) => (
             <Marker
               description={sensor.description}
               key={`sensor-${id}`}
