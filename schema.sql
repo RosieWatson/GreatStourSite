c397fc4edbf4de6e086a8cb18a02b366e541a25d
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-12 14:37:11 +0000

    Updating backfill script

--- schema.sql
+++ schema.sql
@@ -32,7 +32,7 @@ CREATE TABLE `govSensors` (
   `timestamp` int(11) NOT NULL,
   `parameter` varchar(20) DEFAULT NULL,
   `qualifier` varchar(30) DEFAULT NULL,
-  `stationId` varchar(10) DEFAULT NULL,
+  `stationId` varchar(50) DEFAULT NULL,
   `stationLabel` varchar(40) DEFAULT NULL,
   `value` double DEFAULT NULL,
   `unitName` varchar(10) DEFAULT NULL,
