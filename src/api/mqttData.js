ff329e08898ca0be8c9bfd2970d47f0383b169f6
Rosie Watson <rosie.watson@holidayextras.com>
2018-12-12 15:30:08 +0000

    Fixing mqtt db query to stop timeout

--- src/api/mqttData.js
+++ src/api/mqttData.js
@@ -37,9 +37,15 @@ app.get('/api/mqttdata/fetch/floods', async (req, res) => {
   // Fetches data from the DB (where the flood percentage is more than 70%)
   try {
     result = await db.query(
-      `SELECT * FROM mqttSensors mqS
-        WHERE mqS.timestamp = (SELECT MAX(mqS2.timestamp) FROM mqttSensors mqS2 WHERE mqS2.deviceID = mqS.deviceID)
-        AND floodPercentage > 0.69
+      `SELECT ms.deviceID, ms.deviceTime, ms2.longitude, ms2.latitude, ms2.value, ms2.floodPercentage
+        FROM (
+                SELECT deviceID, MAX(deviceTime) deviceTime
+                FROM mqttSensors
+                GROUP BY deviceID
+              ) ms
+        JOIN mqttSensors ms2 ON (ms2.deviceTime = ms.deviceTime AND ms2.deviceID = ms.deviceID)
+        WHERE ms2.floodPercentage > 0.69
+        GROUP BY ms.deviceID, ms2.longitude, ms2.latitude, ms2.value, ms2.floodPercentage;
        `)
   } catch (e) {
     console.log('Failed to fetch data from mqttSensors table', e)
